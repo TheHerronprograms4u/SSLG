03fb359de089e6750c2504f58bf156bd37b501a2
mark2008242 <markmaghacotdesouza@gmail.com>
2026-04-02 12:49:42 +0800

    fixx

--- api/db.ts
+++ api/db.ts
@@ -1,10 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://hytqudnepysviczrsmmf.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5dHF1ZG5lcHlzdmljenJzbW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNTk4NDAsImV4cCI6MjA5MDYzNTg0MH0.YYzN6L1IPaupozuxAoRBBPl0cAKvUV1WqKd_F3KTSzI';
+const supabaseUrl =
+  process.env.SUPABASE_URL || "https://hytqudnepysviczrsmmf.supabase.co";
+const supabaseKey =
+  process.env.SUPABASE_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5dHF1ZG5lcHlzdmljenJzbW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNTk4NDAsImV4cCI6MjA5MDYzNTg0MH0.YYzN6L1IPaupozuxAoRBBPl0cAKvUV1WqKd_F3KTSzI";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
